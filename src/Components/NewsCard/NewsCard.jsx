861bc2651212f462acd157053e75f3fe8bb6ef78
madhulisa <rbanik023@gmail.com>
2023-03-02 11:29:35 +0530

    news card design updated

--- src/Components/NewsCard/NewsCard.jsx
+++ src/Components/NewsCard/NewsCard.jsx
@@ -1,15 +1,35 @@
 import React from 'react';
-import { MDBCard, MDBCardBody } from 'mdb-react-ui-kit';
+import { MDBCard, MDBCardBody, MDBCardHeader, MDBCardFooter, MDBCardTitle, MDBCardText } from 'mdb-react-ui-kit';
 
 export default function NewsCard() {
     return (
         <>
-            <MDBCard>
-                <MDBCardBody>This is some text within a card body.</MDBCardBody>
-            </MDBCard>
-            <MDBCard>
-                <MDBCardBody>This is some text within a card body.</MDBCardBody>
-            </MDBCard>
+            <div className='d-flex justify-content-center'>
+                <MDBCard alignment='center'>
+                    <MDBCardHeader>Featured</MDBCardHeader>
+                    <MDBCardBody>
+                        <MDBCardTitle>Special title treatment</MDBCardTitle>
+                        <MDBCardText>With supporting text below as a natural lead-in to additional content.</MDBCardText>
+                    </MDBCardBody>
+                    <MDBCardFooter className='text-muted'>2 days ago</MDBCardFooter>
+                </MDBCard>
+                <MDBCard alignment='center'>
+                    <MDBCardHeader>Featured</MDBCardHeader>
+                    <MDBCardBody>
+                        <MDBCardTitle>Special title treatment</MDBCardTitle>
+                        <MDBCardText>With supporting text below as a natural lead-in to additional content.</MDBCardText>
+                    </MDBCardBody>
+                    <MDBCardFooter className='text-muted'>2 days ago</MDBCardFooter>
+                </MDBCard>
+                <MDBCard alignment='center'>
+                    <MDBCardHeader>Featured</MDBCardHeader>
+                    <MDBCardBody>
+                        <MDBCardTitle>Special title treatment</MDBCardTitle>
+                        <MDBCardText>With supporting text below as a natural lead-in to additional content.</MDBCardText>
+                    </MDBCardBody>
+                    <MDBCardFooter className='text-muted'>2 days ago</MDBCardFooter>
+                </MDBCard>
+            </div>
         </>
     );
 }
